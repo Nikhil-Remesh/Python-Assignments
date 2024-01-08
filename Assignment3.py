d6f3b5563a013e9cd61e2996ed0323d28396656f
Nikhil-Remesh <148523543+Nikhil-Remesh@users.noreply.github.com>
2024-01-08 22:37:45 +0530

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,21 @@
+class Person:
+    def __init__(self):
+        self.name="nikhil"
+        self.age=10
+        self.address="hai"
+    def update(self):
+        name=input("Enter new name: ")
+        age=input("Enter new age: ")
+        address=input("Enter new Address: ")
+        self.name=name
+        self.age=age
+        self.address=address
+    def display(self):
+        print("the name is ",self.name)
+        print("the age is ",self.age)
+        print("the address is ",self.address)
+        
+p=Person()
+p.display()
+p.update()
+p.display()
